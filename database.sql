7a1d3992c2aca4ba831beb5ed477a5f1e21a0651
Siv Earley <searley96@gmail.com>
2023-03-14 18:09:42 -0500

    updated database.sql with more data

--- database.sql
+++ database.sql
@@ -1,11 +1,21 @@
---create tasks table
+--create table
 CREATE TABLE "tasks" (
-  "id" serial primary key,
-  "task" varchar(200),
-  "complete" boolean
+	"id" serial primary key,
+	"name" varchar(80) not null,
+	"completed" boolean default false
 );
 
 
+
 --test data
-INSERT INTO "tasks" ("task", "complete")
-VALUES ('wash dishes', false);
+INSERT INTO "tasks" ("name", "completed") 
+VALUES
+		('laundry', TRUE),
+		('dishes', FALSE),
+		('clean bathroom', TRUE),
+		('clean bedroom', FALSE),
+		('empty litterbox', TRUE),
+		('feed cat', TRUE),
+		('sweep floor', FALSE),
+		('reset fridge', TRUE),
+		('take out trash', FALSE);
